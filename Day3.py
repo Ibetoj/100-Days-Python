d2894a1d0b8b90a3f0209a3650cd8517bf6daec0
ibetoj <ibetoj@gmail.com>
2024-07-03 09:39:11 +0100

    Learningconcatenation

---
+++ Day3.py
@@ -0,0 +1,9 @@
+yourFood = input("Name a food >")
+yourPlant = input("Name a type of plant >")
+yourCooking = input("Name a method of cooking >")
+
+yourBurned = input("What word describes burned food? >")
+yourDIY = input("Name a DIY item >")
+
+print("MENU")
+print(yourCooking, yourFood, "with", yourBurned, yourPlant, "on a bed of", yourDIY)
