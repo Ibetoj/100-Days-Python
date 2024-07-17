ffb9adaa673c8ed8a7af443742c55b90d2ba1a8b
ibetoj <ibetoj@gmail.com>
2024-07-17 02:14:06 +0100

    if statement 1

---
+++ Day5.py
@@ -0,0 +1,6 @@
+print(" === Marvel Movie Character Creator === ")
+hanging = input("Do you like 'hanging' around?:")
+if hanging == "No":
+    print("Then you not Spider-man")
+else:
+    print("You're Spider-man")
