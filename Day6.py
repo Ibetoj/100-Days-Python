0538dfc35014acb2f08a2ef04d8b800c44c76f55
ibetoj <ibetoj@gmail.com>
2024-08-05 05:17:55 +0100

    if and elif

---
+++ Day6.py
@@ -0,0 +1,15 @@
+print("MY LOGIN SYSTEM")
+username = input("Username >")
+password = input("Password >")
+if username == "David" and password == "totallyNotBald":
+    print("Why hello there david, what a lovely accent you have, you could charmed your in here even without password")
+
+    print("Have a great day.Don't forget wear a hat in the sun!")
+elif username == "Nkechi" and password == "Nk8chi":
+    print("Hello Nkechi, you are welcome here")
+elif username == "John" and password == "John123":
+    print("You look handsome today John and you're welcome to Ibeto Investment")
+elif username == "James" and password == "James1234":
+    print("Welcome James, have a great day")
+else:
+    print("You're not welcome here, please leave")
